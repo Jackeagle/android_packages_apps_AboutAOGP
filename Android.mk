35b85eea77b6d854b07d2cdef22ff22613a4cfe6
Shubhang Rathore <shubhang92@gmail.com>
2014-08-02 16:25:14 +0530

    Android.mk: change local package name to 'AboutAOGP'
    The string resource has the app name as About AOGP (TM).
    This is in compliance with the app name.
    Also updated the repo name to match the app name.

--- Android.mk
+++ Android.mk
@@ -24,7 +24,7 @@ LOCAL_STATIC_JAVA_LIBRARIES := android-support-v4
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
-LOCAL_PACKAGE_NAME := AOGPAbout
+LOCAL_PACKAGE_NAME := AboutAOGP
 
 include $(BUILD_PACKAGE)
 ##################################################
